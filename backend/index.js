f8014f184da3f94d3658703b892612364955b3b7
syed-bilalahmed <syedbilalahmed5292@gmail.com>
2024-06-13 13:17:58 +0500

    add another middle ware

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express=require('express');
 const app=express();    
 const port=8000;
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/register', (req, res) =>{
     let {username,password} = req.query;
