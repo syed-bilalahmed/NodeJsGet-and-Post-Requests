6d1c02a8f9f5bdd775e3cd4a05f5e1dabb106b5a
syed-bilalahmed <syedbilalahmed5292@gmail.com>
2024-06-13 13:15:14 +0500

    add mildlleware

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 const express=require('express');
 
 const app=express();    
-const port=8080;
+const port=8000;
+app.use(express.urlencoded({extended: true}));
+
+app.get('/register', (req, res) =>{
+    let {username,password} = req.query;
+
+res.send(`Standard Get Request Welcome : @${username}`);   
+});
+
+app.post('/register', (req, res) =>{
+    let{username,password} = req.body; 
+
+    res.send(`Standard Post Request Welcome : @${username}`)  ; 
+    });
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
 });
